c30f3d79bf7c6f0f6f1d3194cd321dbe11d674f1
Marco Lopes <marco.paulo.lopes@gmail.com>
2018-02-28 22:14:44 +0000

    Create map, mines and tavern lookup

--- src/main/scala/Bots.scala
+++ src/main/scala/Bots.scala
@@ -1,6 +1,7 @@
 package bot
 
 import Dir._
+
 import scala.util.Random
 import Tile._
 
@@ -10,9 +11,41 @@ trait Bot {
 
 class RandomBot extends Bot {
 
-  def move(input: Input) = {
+  var mines: Vector[(Pos, Tile)] = Vector.empty
+  var taverns: Vector[(Pos, Tile)] = Vector.empty
+  var worldMap: Vector[(Pos, Tile)] = Vector.empty
+
+  def move(input: Input): bot.Dir.Value = {
+
+    if(input.game.turn < 4) {
+      buildLookups(input.game.board)
+    }
+
     Random.shuffle(List(Dir.North, Dir.South, Dir.East, Dir.West)) find { dir ⇒
       input.game.board at input.hero.pos.to(dir) exists (Wall!=)
     }
   } getOrElse Dir.Stay
+
+
+  private def buildLookups(gameBoard: Board): Unit = {
+    buildBoard(gameBoard)
+    buildMinesLookup
+    buildTavernsLookup
+  }
+
+  private def buildBoard(board: Board): Unit =
+    worldMap = board.tiles.zipWithIndex.map { indexedTile: (Tile, Int) =>
+      (
+        Pos(indexedTile._2 / board.size, indexedTile._2 % board.size),
+        indexedTile._1 match {
+          case Tile.Hero(_) => Tile.Air
+          case t: Tile => t })}
+
+  private def buildTavernsLookup: Unit = taverns = worldMap.filter {
+      case (_, Tile.Tavern) => true
+      case _ => false }
+
+  private def buildMinesLookup: Unit = mines = worldMap.filter {
+      case (_, Tile.Mine(owner)) => true
+      case _ => false }
 }
